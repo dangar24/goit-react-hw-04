dfd4c2c75142bd9db02a2aeb47f93c99df1aef94
dangar24 <danil.gavrilenko24@gmail.com>
2024-07-07 15:54:35 +0300

    litlFix

--- src/Components/ImageModal/ImageModal.jsx
+++ src/Components/ImageModal/ImageModal.jsx
@@ -8,18 +8,14 @@ const customStyles = {
         zIndex: 999,
     },
     content: {
-        top: '10%', // Закріплюємо зверху
-        left: '10%', // Закріплюємо зліва
-        // right: '10%', // Закріплюємо справа
-        // bottom: '10%', // Закріплюємо знизу
-        border: 'none', // Без рамки
-        background: 'transparent', // Прозорий фон
-        overflow: 'hidden', // Забороняємо прокручування контенту
-    display: 'flex', // Використовуємо flexbox
-    justifyContent: 'center', // Вирівнюємо по центру
-    alignItems: 'center', // Вирівнюємо по центру
-    padding: '0',
-    paddingTop: '',
+        top: '10%', 
+        left: '10%', 
+        border: 'none', 
+        background: 'transparent', 
+        overflow: 'hidden', 
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
     zIndex: '999',
 },
 };
